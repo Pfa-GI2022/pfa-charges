070117c747c5ff3c6c116054bd98e7387f6c929a
Equinox <flay_power@hotmail.fr>
2021-04-27 13:51:37 +0000

    POST Routes configured for testing

--- api/routes/matieres.js
+++ api/routes/matieres.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 const Sequelize = require("sequelize");
 const db = require("../models/index");
 const matieres = db.matiere;
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 //GET ALL
 router.get("/", (req, res) => {
@@ -16,7 +20,13 @@ router.get("/:name", (req, res) => {
 });
 
 //CREATE
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  let info = req.body.info;
+  let someInfo = req.body.someInfo;
+  let someOtherInfo = req.body.someOtherInfo;
+
+  res.send(`Field 1: ${info}, Field 2: ${someInfo}, Field 3: ${someOtherInfo}`);
+});
 
 //UPDATE
 router.put("/:name", (req, res) => {});
--- api/routes/professeurs.js
+++ api/routes/professeurs.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 const db = require("../models/index");
 const chargeRouter = require("./charges");
 const prof = db.professeur;
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 //GET ALL
 router.get("/", (req, res) => {
@@ -17,7 +21,13 @@ router.get("/:name", (req, res) => {
 
 router.use("/:name/charge", chargeRouter);
 //CREATE
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  let info = req.body.info;
+  let someInfo = req.body.someInfo;
+  let someOtherInfo = req.body.someOtherInfo;
+
+  res.send(`Field 1: ${info}, Field 2: ${someInfo}, Field 3: ${someOtherInfo}`);
+});
 
 //UPDATE
 router.put("/:name", (req, res) => {});
