92f28e120ca3c2a1978059757c5bdf0a1d9316c5
Equinox <mouaad.mh.hafid@gmail.com>
2021-05-30 14:02:39 +0100

    Login Component added

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,21 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListeProfesseursComponent } from './components/liste-professeurs/liste-professeurs.component';
 import { FiliereService } from './services/filiere.service';
+import { LoginComponent } from './components/login/login.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
-    ListeProfesseursComponent
+    ListeProfesseursComponent,
+    LoginComponent,
+    SidenavComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    HttpClientModule
-  ],
-  providers: [
-    FiliereService
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, RouterModule.forRoot(routes), HttpClientModule],
+  providers: [FiliereService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
