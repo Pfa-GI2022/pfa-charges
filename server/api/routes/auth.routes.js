d317fd5440b954bfce8f20e960df7a27f2a0eda1
Equinox <mouaad.mh.hafid@gmail.com>
2021-06-26 15:24:57 +0100

    Backend Fix

--- server/api/routes/auth.routes.js
+++ server/api/routes/auth.routes.js
@@ -2,19 +2,19 @@ const verifySignUp = require("../middlewares/verifySignUp");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function (app) {
-  app.use(function (req, res, next) {
-    //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-  });
+  // app.use(function (req, res, next) {
+  //   //Enabling CORS
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header(
+  //   "Access-Control-Allow-Methods",
+  //   "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  // );
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  // );
+  // next();
+  // });
 
   app.post(
     "/api/auth/signup",
--- server/server.js
+++ server/server.js
@@ -8,19 +8,7 @@ var corsOptions = { origin: "http://localhost:3000" };
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+app.use(cors());
 
 //importing the routes
 const departementRoutes = require("./api/routes/departements");
